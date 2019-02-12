a7a6fff1c0571c490df189ed380b04ac36aac553
Johan Henrik <johandragsten0@gmail.com>
2019-02-12 14:47:29 +0100

    Updates small fixes.

--- week2_programming_exercises/oppgave2.c
+++ week2_programming_exercises/oppgave2.c
@@ -16,13 +16,18 @@ struct person * person_create(unsigned int count/*int age, char name[]*/) {
     strncpy(my_person[0].name, "Johan Henrik", 20);
     my_person[1].age = 21;
     strncpy(my_person[1].name, "Natalie", 20);
+    my_person[2].age = 62;
+    strncpy(my_person[2].name, "Ingunn", 20);
+    my_person[3].age = 18;
+    strncpy(my_person[3].name, "Aili", 20);
     return my_person;
 }
 
 int main() {
-    struct person * my_persons = person_create(2);
+    int percou = 4;
+    struct person * my_persons = person_create(percou);
     int i;
-    for (i=0; i<2; i++) {
+    for (i=0; i<percou; i++) {
         printf("Person [age - %d, name - %s]\n", my_persons[i].age,
             my_persons[i].name);
     }
